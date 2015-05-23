4bf67b5776985c0674582c592c5a66a8103eec91
Top <Top@Top-PC>
2015-05-23 14:33:25 +0700

    aaaa

---
+++ Example01/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+public static void main(String[] args)
+{
+	Rectangle rect = new Rectangle();
+	Rectangle rectb= new Rectangle();
+	rect.setValues(3,4);
+	rect.setValues(5,6);
+	System.out.println("rect a:"+ rect.area());
+	System.out.println("rect b:"+ rectb.area());
+	}
+}
---
+++ Example01/src/Rectangle.java
@@ -0,0 +1,15 @@
+
+public class Rectangle {
+	int x,y;
+	public void setValues(int a,int b)
+	{
+		x=a;
+		y=b;
+	}
+	public int area()
+	{
+		return(x*y);
+	}
+	
+
+}

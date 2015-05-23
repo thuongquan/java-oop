98ca3b3010d95b4dd0aac001525fe2361997d70f
Top <Top@Top-PC>
2015-05-23 14:34:36 +0700

    2

---
+++ Example02/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Rectangle recta=new Rectangle(3,4);
+		Rectangle rectb=new Rectangle(5,6);
+		System.out.println("rect a:"+recta.area());
+		System.out.println("rect b:"+rectb.area());
+		
+	}
+
+}
---
+++ Example02/src/Rectangle.java
@@ -0,0 +1,15 @@
+
+public class Rectangle {
+	int width,height;
+	public Rectangle(int a,int b)
+	{
+		width=a;
+		height=b;
+	}
+	public int area()
+	{
+		return (width*height);
+	}
+
+	
+}
